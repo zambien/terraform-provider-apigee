e4455a86afdd82039323cedcf17a0d231d9f54e8
Mcneely <zambien1977@yahoo.com>
2017-08-17 09:15:26 -0400

    readme and naming updates

--- README.md
+++ README.md
@@ -1,33 +1,40 @@
-# terraform-provider-apigee
+# terraform-provider-apigee [WIP]
 A Terraform Apigee provider focused on Products and Proxies.
 
-## Required TFVARS for provider
+This is a work in progress.  Once it is to the point that is solves the use cases of proxy create and deploy I will submit PR to the terraform-providers repo.
+
+## TFVARS for provider
 
 ```
-APIGEE_ORG=user-org-name
-APIGEE_USER_EMAIL=user@email.com
-APIGEE_PASS=youknowwhatgoeshere
+APIGEE_BASE_URI="https://someinternalapigee.yourdomain.wtf" # optional... defaults to Apigee's SaaS
+APIGEE_ORG="user-org-name"
+APIGEE_USER="user@email.com"
+APIGEE_PASSWORD="fortheloveofpetepleaseuseastrongpassword"
 ```
 
-## Example
-
+## Simple Example
 
 ```
 provider "apigee" {
+  org="my_really_cool_org_name"
+  user="some_dude@domain.wtf"
+  password="didupickastrongone?" # Generally speaking, don't put passwords in your tf files... pull from a Vault or something.
 }
 
 resource "apigee_api_proxy" "helloworld_proxy" {
-   name_prefix =  "helloworld-terraformed"
-   bundle = "${data.archive_file.bundle.output_path}"
-   bundle_sha = "${data.archive_file.bundle.output_sha}"
+   name_prefix =  "helloworld-terraformed" # used to prepend your proxy name.  Name will be name_prefix + a uuid.
+   bundle = "${data.archive_file.bundle.output_path}" # Apigee APIs require a zip bundle to import a proxy.
+   bundle_sha = "${data.archive_file.bundle.output_sha}" # The SHA is used to detect changes for plan/apply.
 }
 
+# This is a normal terraform offering and serves as an example of how you might create a proxy bundle.
 data "archive_file" "bundle" {
    type        = "zip"
    source_dir = "${path.module}/proxy_files"
    output_path = "${path.module}/proxy_files_bundle/apiproxy.zip"
 }
 
+# Outputs
 output "apigee_api_proxy_name" {
    value = ["${apigee_api_proxy.helloworld_proxy.name}"]
 }
--- provider.go
+++ provider.go
@@ -4,30 +4,29 @@ package main
 import (
 	"github.com/hashicorp/terraform/helper/schema"
 	"github.com/hashicorp/terraform/terraform"
-	"log"
 	"github.com/zambien/go-apigee-edge"
 )
 
 func Provider() terraform.ResourceProvider {
 	return &schema.Provider{
 		Schema: map[string]*schema.Schema{
-			"baseUri": &schema.Schema{
+			"base_uri": &schema.Schema{
 				Type: schema.TypeString,
 				Optional: true,
 				DefaultFunc: schema.EnvDefaultFunc("APIGEE_BASE_URI", nil),
 				Description: "Apigee Edge Base URI",
 			},
-			"userEmail": &schema.Schema{
+			"user": &schema.Schema{
 				Type: schema.TypeString,
 				Required: true,
-				DefaultFunc: schema.EnvDefaultFunc("APIGEE_USER_EMAIL", nil),
-				Description: "Apigee Email Address",
+				DefaultFunc: schema.EnvDefaultFunc("APIGEE_USER", nil),
+				Description: "Apigee User",
 			},
-			"pass": &schema.Schema{
+			"password": &schema.Schema{
 				Type: schema.TypeString,
 				Required: true,
-				DefaultFunc: schema.EnvDefaultFunc("APIGEE_PASS", nil),
-				Description: "Apigee Email Password",
+				DefaultFunc: schema.EnvDefaultFunc("APIGEE_PASSWORD", nil),
+				Description: "Apigee User Password",
 			},
 			"org": &schema.Schema{
 				Type: schema.TypeString,
@@ -48,28 +47,16 @@ func Provider() terraform.ResourceProvider {
 func configureProvider(d *schema.ResourceData) (interface{}, error) {
 
 	auth := apigee.EdgeAuth{
-		Username: d.Get("userEmail").(string),
-		Password: d.Get("pass").(string),
+		Username: d.Get("user").(string),
+		Password: d.Get("password").(string),
 	}
 
-	/*
-	config := Config{
-		BaseURI : d.Get("baseUri").(string),
-		Auth: authm,
-		User: d.Get("userEmail").(string),
-		Pass: d.Get("pass").(string),
-		Org: d.Get("org").(string),
-	}*/
-
-	log.Println("[INFO] Initializing Apigee client")
-	log.Printf("[INFO] Logging in with user: %#v\n", auth.Username)
-
 	opts := &apigee.EdgeClientOptions{
-		MgmtUrl : d.Get("baseUri").(string),
+		MgmtUrl : d.Get("base_uri").(string),
 		Org: d.Get("org").(string),
 		Auth: &auth, Debug: false,
 	}
-	//client, err := config.Client()
+
 	client, err := apigee.NewEdgeClient(opts)
 	if err != nil {
 		return client, err
